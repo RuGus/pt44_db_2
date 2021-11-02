704f25950248f2350ee657e2913d85c32a8ffa7d
Руслан Гусманов <35694691+RuGus@users.noreply.github.com>
2021-11-02 17:41:22 +0300

    Insert script template

---
+++ task_1_insert_values_script.sql
@@ -0,0 +1,49 @@
+INSERT INTO Genres (Name) VALUES 
+    ('Blues'),
+    ('Disco'),
+    ('Reggy'),
+    ('Ambient'),
+    ('Pop');
+INSERT INTO Singers (Name) VALUES 
+    ('Elvis Presley'),
+    ('Bob Marley'),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    ('');
+INSERT INTO Albums (Name, Year) VALUES 
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    ('');
+INSERT INTO Tracks (Name, Duration, Album_id) VALUES 
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    ('');
+INSERT INTO Collections (Name, Year) VALUES 
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    ('');
